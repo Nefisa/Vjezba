65381bf5ebf05917149018efd83d7afb3a12f268
Elmin Hodzic <sh3min3m@gmail.com>
2018-10-19 17:58:41 +0200

    metoda za neparne projeve

--- src/bezidejno.java
+++ src/bezidejno.java
@@ -7,6 +7,16 @@ public class bezidejno {
 	
 	public void ispisiNeparneBrojeve(int a) {
 		
+	    
+	    for (int i = 0; i < a; i++) {
+		
+		if ( i % a != 0) {
+		    
+		    System.out.println(i);
+		}
+		
+	    }
+	    
 	}
 	
 	public void ispisiPrimeBrojeve(int a) {
