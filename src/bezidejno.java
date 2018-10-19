0497bbfec4605b9c4f61db3a6b760a8abdeacb3b
Nefisa Brčaninović <nefisa.h@hotmail.com>
2018-10-19 17:46:00 +0200

    nesto

---
+++ src/bezidejno.java
@@ -0,0 +1,20 @@
+
+public class bezidejno {
+	
+	public void ispisiParneBrojeve(int a) {
+		
+	}
+	
+	public void ispisiNeparneBrojeve(int a) {
+		
+	}
+	
+	public void ispisiPrimeBrojeve(int a) {
+		
+	}
+	
+	public void ispisiUnazad(int a) {
+		
+	}
+
+}
