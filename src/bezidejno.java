e02cd07b53b7539d4ce81280882a66bc69ef516c
Mersudin Hajdarevic <h.mersudin1996@gmail.com>
2018-10-20 21:45:26 +0200

    implemented prime

--- src/bezidejno.java
+++ src/bezidejno.java
@@ -10,7 +10,22 @@ public class bezidejno {
 	}
 	
 	public void ispisiPrimeBrojeve(int a) {
-		
+
+		boolean prime=false;
+		for(int i=2; i<=100; i++) {
+			for(int j=2; j<100; j++) {
+				if(i%j==0 && i!=j) {
+					prime=false;
+					break;
+				}
+				else {
+					prime=true;
+					continue;
+				}
+			}
+			if(prime)
+				System.out.println(i);
+		}
 	}
 	
 	public void ispisiUnazad(int a) {
